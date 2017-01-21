13a9d57d7f15396e8881f79853fc97b17df43a13
bgsrb88 <bgsrb88@gmail.com>
2017-01-21 11:20:32 +0330

    Update

--- Kavenegar/Program.cs
+++ Kavenegar/Program.cs
@@ -5,80 +5,75 @@ namespace Kavenegar
  using com.kavenegar.api;
  class Program
  {
-	public const string Apikey = "your Apikey";
-	private const string Sender = "10006707323323";
+	public const string Apikey = "Your APIKey";
+	private const string Sender = "Your Sender Number";
 	private static v1SoapClient _client;
 
 	static void Main()
 	{
 	 _client = new v1SoapClient();
-	 var requestBody = new SendSimpleByApikeyRequestBody
-	 {
-		apikey = Apikey,
-		message = "خدمات پیام کوتاه کاوه نگار",
-		msgmode = 0,
-		receptor = new ArrayOfString { " receptor" },
-		sender = Sender,
-		status = 0,
-		statusmessage = "",
-		unixdate = 0
-	 };
-	 var sendResult = Send(requestBody);
-	 if (sendResult == null) return;
-	 foreach (var smsRefrenceId in sendResult)
-	 {
-		Console.WriteLine("SmsRefrenceID:" + smsRefrenceId);
-	 }
-	 var ids = new ArrayOfLong();
-	 ids.AddRange(sendResult);
-	 var smsstatus = Status(ids);
-	 foreach (var st in smsstatus)
-	 {
-		switch (st)
-		{
-		 case 1:
-			Console.WriteLine("Queued to send");
-			break;
-		 case 2:
-			Console.WriteLine("Scheduled");
-			break;
-		 case 4:
-			Console.WriteLine("Sent to Operator");
-			break;
-		 case 10:
-			Console.WriteLine("Sent to Receptor (Delivered)");
-			break;
-		 case 11:
-			Console.WriteLine("Undelivered");
-			break;
-		 case 14:
-			Console.WriteLine("Receptor number has been blocked..");
-			break;
-		 case 100:
-			Console.WriteLine("Refrence ID not found");
-			break;
-		}
-	 }
+		//Send();
+		ReceiveByApikey();
 	 Console.WriteLine("Press Enter to close");
 	 Console.ReadKey();
 	}
 
-	public static ArrayOfLong Send(SendSimpleByApikeyRequestBody requestBody)
+	public static void Send()
 	{
 	 try
 	 {
-		var result = _client.SendSimpleByApikey(requestBody.apikey, requestBody.sender, requestBody.message, requestBody.receptor, requestBody.unixdate, requestBody.msgmode, ref requestBody.status, ref requestBody.statusmessage);
-		if (requestBody.status != 200)
+		var requestBody = new SendSimpleByApikeyRequestBody
 		{
-		 Console.WriteLine("Returned [{0}] Message:[{1}]", requestBody.status, requestBody.statusmessage);
-		 return null;
-		}
-		else return result;
+		 apikey = Apikey,
+		 message = "خدمات پیام کوتاه کاوه نگار",
+		 msgmode = 0,
+		 receptor = new ArrayOfString { "receptor" },
+		 sender = Sender,
+		 status = 0,
+		 statusmessage = "",
+		 unixdate = 0
+		};
+		var result = _client.SendSimpleByApikey(requestBody.apikey, requestBody.sender, requestBody.message, requestBody.receptor, requestBody.unixdate, requestBody.msgmode, ref requestBody.status, ref requestBody.statusmessage);
+		 if (requestBody.status != 200)
+		 {
+			 Console.WriteLine("Returned [{0}] Message:[{1}]", requestBody.status, requestBody.statusmessage);
+		 }
+		 else if (result != null)
+		 {
+			var smsstatus = Status(result);
+			foreach (var st in smsstatus)
+			{
+			 switch (st)
+			 {
+				case 1:
+				 Console.WriteLine("Queued to send");
+				 break;
+				case 2:
+				 Console.WriteLine("Scheduled");
+				 break;
+				case 4:
+				 Console.WriteLine("Sent to Operator");
+				 break;
+				case 10:
+				 Console.WriteLine("Sent to Receptor (Delivered)");
+				 break;
+				case 11:
+				 Console.WriteLine("Undelivered");
+				 break;
+				case 14:
+				 Console.WriteLine("Receptor number has been blocked..");
+				 break;
+				case 100:
+				 Console.WriteLine("Refrence ID not found");
+				 break;
+			 }
+			}
+		 }
+		 
 	 }
 	 catch (Exception ex)
 	 {
 		Console.WriteLine(ex.Message);
-		return null;
 	 }
 	}
 
@@ -102,5 +97,33 @@ namespace Kavenegar
 		return null;
 	 }
 	}
+
+	public static void ReceiveByApikey()
+	{
+	 try
+	 {
+		var requestBody = new ReceiveByApikeyRequestBody()
+		{
+		  apikey = Apikey,
+			lineNumber = Sender,
+      isread = 1,
+		  status = 0,
+		  statusmessage = ""
+		};
+		var result = _client.ReceiveByApikey(requestBody.apikey, requestBody.lineNumber,requestBody.isread, ref requestBody.status, ref requestBody.statusmessage);
+		 if (result != null)
+		 {
+			foreach (var r in result)
+			{
+			 Console.Write("{0} {1} {2}", r.sender, r.message, Environment.NewLine);
+			}
+		 }	
+	 }
+	 catch (Exception ex)
+	 {
+		Console.Write(ex.Message);
+	 }
+	}
+
  }
 }
